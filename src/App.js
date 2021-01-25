63793682496ddf40f0bac1c47b6ed40d61af5fb6
Seanbon0611 <seand0611@gmail.com>
2021-01-24 20:12:50 -0800

    added React Router to Project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Header from "./components/Header";
 import LandingPage from "./pages/LandingPage";
 
 const App = () => {
   return (
     <div>
-      <LandingPage />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+      </Switch>
     </div>
   );
 };
