49db3a87a4a27b6fd9102f06b1f3d664d10164d7
Seanbon0611 <seand0611@gmail.com>
2021-01-24 19:54:48 -0800

    initialing application with tailwindcss

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h1 className="text-5xl text-gray-800">Hello, Welcome to Exotrainer</h1>
+    </div>
+  )
+}
+
+export default LandingPage
